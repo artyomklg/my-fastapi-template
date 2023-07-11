9e290ccd523a197d30c44216c6cc4e18d89fd545
artyomklg <artem0801200269@gmail.com>
2023-07-11 16:33:25 +0300

    update fastapi and sqlalchemy, update config

--- src/config.py
+++ src/config.py
@@ -1,32 +1,44 @@
-import pathlib
-from typing import List
+from typing import List, Literal
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .constants import Environment
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+    LOG_LEVEL: str
+
     POSTGRES_DB: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_PORT: str
 
-    SECRET: str
-    ALGORITM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 20
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
-    ENVIRONMENT: Environment = Environment.PRODUCTION
+    TEST_POSTGRES_DB: str
+    TEST_POSTGRES_USER: str
+    TEST_POSTGRES_PASSWORD: str
+    TEST_POSTGRES_HOST: str
+    TEST_POSTGRES_PORT: str
+
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_POSTGRES_USER}:{self.TEST_POSTGRES_PASSWORD}@{self.TEST_POSTGRES_HOST}:{self.TEST_POSTGRES_PORT}/{self.TEST_POSTGRES_DB}"
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
 
     CORS_ORIGINS: List[str]
     CORS_HEADERS: List[str]
     CORS_METHODS: List[str]
 
-    @property
-    def Database_URL(self):
-        return f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
 
 settings: Settings = Settings()
